8b1c34de9a0e0a7c24d28c6891ad698b56c9372a
David Schmidt <david__schmidt@users.sourceforge.net>
2013-09-29 04:37:11 +0000

    Clean up an assembler warning

--- client/src/timetest.asm
+++ client/src/timetest.asm
@@ -378,10 +378,10 @@ ReadBlockPhaseBegin:
 	lda NUMBLKS+1
 	sta <dividend+1
 	; Need to divide the dividend by 2... since we currently hold blocks, which are 512 bytes and not 1K.
-	lda dividend+1	; Load the MSB
+	lda <dividend+1	; Load the MSB
         asl a		; Copy the sign bit into C
-        ror dividend+1	; And back into the MSB
-        ror dividend	; Rotate the LSB as normal
+        ror <dividend+1	; And back into the MSB
+        ror <dividend	; Rotate the LSB as normal
 	ldx #$1f
 	ldy #$06
 	jsr GOTOXY
